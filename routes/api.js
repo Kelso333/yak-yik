80c016735dee8eeef77631047dfe5d16fe923326
Rosibel Cisneros <kelsocisneros@gmail.com>
2017-10-18 11:32:18 -0700

    updated api.js

--- routes/api.js
+++ routes/api.js
@@ -3,6 +3,12 @@ var router = express.Router();
 
 router.get('/:resource', function(req, res, next) {
   
+  var resource = req.params.resource;
+  
+  res.json({
+    confirmation: 'success',
+    resource: resource
+  })
 })
 
 
