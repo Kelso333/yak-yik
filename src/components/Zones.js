95febd55d3de523495f917d8c0072319760fa23e
Rosibel Cisneros <kelsocisneros@gmail.com>
2017-10-23 16:26:27 -0700

    added Zones component

---
+++ src/components/Zones.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+class Zones extends Component {
+  render() {
+    return (
+
+      <div>
+        Zones!!!
+      </div>
+
+    )
+  }
+}
+
+export default Zones
