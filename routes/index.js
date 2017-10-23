9c9647666cde1f8a5eb6ed8453f619ccdba1241b
Rosibel Cisneros <kelsocisneros@gmail.com>
2017-10-22 20:59:15 -0700

    installed all modules manually

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
@@ -8,6 +9,10 @@ router.get('/', function(req, res, next) {
 
 router.get('/createzone', function(req, res, next) {
   res.render('createzone', null);
+});
+
+router.get('/createcomment', function(req, res, next) {
+  res.render('createcomment', null);
 });
 
 module.exports = router;
